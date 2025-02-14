52124434373488934171f64f18082f6f9b095ebe
ashiyaman <ashiya1005@gmail.com>
2025-02-15 01:39:24 +0530

    feat: Get All events from db

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,15 +1,14 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv').config();
 
-const MONGODB_URI = process.env.MONGODB_URI
-console.log('MongoDB URI:', process.env.MONGODB_URI);
+const mongoose = require("mongoose")
+
+const mongoUri = process.env.MONGODB_URI
 
 const initializeDatabase = async () => {
-        console.log(MONGODB_URI)
-     await mongoose
-        .connect(MONGODB_URI)
-        .then(() => console.log('Connected to Database'))
-        .catch((error) => console.log('Error while connecting to Database.', error))
+    await mongoose
+    .connect(mongoUri)
+    .then(() => console.log("Connected to Database"))
+    .catch((error) => console.log("Error connecting to database", error))
 }
 
 module.exports = {initializeDatabase}
--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.get('/events', async(req, res) => {
     }
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
     console.log('App running on port ', PORT)
 })
