6876d4e250f4b844d2d2b0d4d433ce045134d996
ashiyaman <ashiya1005@gmail.com>
2025-02-17 17:59:24 +0530

    chore: Add cors configuration

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ const fs = require('fs')
 const express = require('express')
 const {initializeDatabase} = require('./db/db.connect.js')
 
+const app = express()
+app.use(express.json())
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+
 initializeDatabase()
 /*const jsonEventData = fs.readFileSync('./events.json', 'utf-8')
 const eventsData = JSON.parse(jsonEventData)
 const jsonUserData = fs.readFileSync('./users.json', 'utf-8')
 const usersData = JSON.parse(jsonUserData)*/
 
-const app = express()
-app.use(express.json())
+
 
 const seedUserData = () => {
     try{
