4b7eb81b503c56277991067ca60c22a40347eced
ashiyaman <ashiya1005@gmail.com>
2025-02-24 14:32:03 +0530

    Fix: Add session fields

--- models/events.models.js
+++ models/events.models.js
@@ -1,62 +1,74 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const eventSchema = new mongoose.Schema({
+const eventSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     date: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true
+    },
+    sessionStart: {
+      type: Date,  // Start time of the event
+      required: true
+    },
+    sessionEnd: {
+      type: Date,  // End time of the event
+      required: true
     },
     venue: {
-        type: String
+      name: { type: String, required: true },  
+      address: { type: String, required: true }, 
+      city: { type: String, required: true },  
+      state: { type: String },  
+      country: { type: String}, 
     },
     eventType: {
-        type: String,
-        enum: ['Online', 'Offline', 'Both'],
-        required: true
+      type: String,
+      enum: ['Online', 'Offline', 'Both'],
+      required: true
     },
     category: {
-        type: [String],
-        enum: ['Technology', 'Arts', 'Business', 'Others']
+      type: [String],
+      enum: ['Technology', 'Arts', 'Business', 'Others']
     },
-
     images: [String],
     tags: [String],
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     isPaid: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     price: Number,
     numberOfAttendees: {
-        type: Number,
-        min: 0
+      type: Number,
+      min: 0
     },
     dressCode: String,
     eventUrl: String,
     requiredAge: {
-        type: String,
-        enum: ['18 and above', '18 - 25', '25-45', 'below 60']
+      type: String,
+      enum: ['18 and above', '18 - 25', '25-45', 'below 60']
     },
     status: {
-        type: String,
-        enum: ['Upcoming', 'Live', 'Completed', 'Postponed', 'Canceled'],
-        default: 'Upcoming'
+      type: String,
+      enum: ['Upcoming', 'Live', 'Completed', 'Postponed', 'Canceled'],
+      default: 'Upcoming'
     },
     capacity: {
-        type: Number,
-        min: 0,
-        required: true
+      type: Number,
+      min: 0
     },
-    presenters: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
-},
-{timestamps: true})
+    presenters: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  },
+  { timestamps: true }
+);
 
-const Events = mongoose.model('Events', eventSchema)
+const Events = mongoose.model('Events', eventSchema);
 
-module.exports = Events
+module.exports = Events;
